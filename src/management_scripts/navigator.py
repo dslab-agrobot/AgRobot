1a063822788f3c3b4291f9ba2d1a194e8ab055de
JChrysanthemum <624099049@qq.com>
2019-05-15 15:46:03 +0800

    add navigator to control robot

--- src/management_scripts/navigator.py
+++ src/management_scripts/navigator.py
@@ -24,60 +24,78 @@ nav.move_y(DISTANCE)
 
 """
 
-import tools.connector
+from tools.connector import RpiArdConnector
 import argparse
 
 
-#
-def move_x(dis):
+class Navigator(object):
+    """Navigator to control the robot
 
-    cmd = "X"
-    if dis >= 0 :
-        if dis < 10 :
-            cmd += '+00'
-        elif dis <100 :
-            cmd += '+0'
+    1). control robot to MOVE forward,backward,rightward,leftward
+    2). control robot to ROTATE the camera
+    3). control robot to LIGHT led light
+
+    Attributes:
+        ser_timeout: time out of response to serial
+
+    """
+
+    def __init__(self):
+        self.conn = RpiArdConnector()
+
+    def __del__(self):
+        del self.conn
+
+    def move_x(self, dis):
+
+        cmd = "X"
+        if dis >= 0:
+            if dis < 10:
+                cmd += '+00'
+            elif dis < 100:
+                cmd += '+0'
+            else:
+                cmd += '+'
         else:
-            cmd += '+'
+            if dis > -10:
+                cmd += '-00'
+            elif dis > -100:
+                cmd += '-0'
+            else:
+                cmd += '-'
+        cmd += str(abs(dis))
+        return self.conn.send_msg(cmd.encode("ascii"))
+
+    def move_y(self, dis):
+        cmd = "Y"
+        if dis >= 0:
+            if dis < 10:
+                cmd += '+00'
+            elif dis < 100:
+                cmd += '+0'
+            else:
+                cmd += '+'
+        else:
+            if dis > -10:
+                cmd += '-00'
+            elif dis > -100:
+                cmd += '-0'
+            else:
+                cmd += '-'
+        cmd += str(abs(dis))
+        return self.conn.send_msg(cmd.encode("ascii"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="direction of it's movement", type=str, choices=['x', 'X', 'y', 'Y'])
+    parser.add_argument("len", help="length of it's movement ", type=int)
+    args = parser.parse_args()
+    nav = Navigator()
+    if args.dir == 'x' or args.dir == 'X':
+        nav.move_x(args.len)
     else:
-        if dis > -10 :
-            cmd += '-00'
-        elif dis > -100:
-            cmd += '-0'
-        else :
-            cmd += '-'
-    cmd += str(abs(dis))
-    print(cmd)
-    # return s.send_cmd(cmd.encode("ascii"))
+        nav.move_y(args.len)
 
 
-
-def move_y(dis):
-    cmd = "Y"
-    if dis >= 0:
-        if dis < 10:
-            cmd += '+00'
-        elif dis < 100:
-            cmd += '+0'
-        else:
-            cmd += '+'
-    else:
-        if dis > -10:
-            cmd += '-00'
-        elif dis > -100:
-            cmd += '-0'
-        else:
-            cmd += '-'
-    cmd += str(abs(dis))
-    print(cmd)
-    # return s.send_cmd(cmd.encode("ascii"))
-
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number", type=int)
-args = parser.parse_args()
-print (args.square**2)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number", type=int)
-args = parser.parse_args()
 

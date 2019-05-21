9d6cd99298d91cf7fd7e89a766b0747c698f4690
魏永强 <2582869579@qq.com>
2019-05-21 21:53:37 +0800

    update, move X and Y to take pics auto

--- src/management_scripts/picAll.py
+++ src/management_scripts/picAll.py
@@ -2,8 +2,8 @@
 Take photos for the whole field
 
 
-__copyright__="Jiangxt"
-__email__ = "<jiangxt404@qq.com>"
+__copyright__="Jiangxt""weiyq"
+__email__ = "<jiangxt404@qq.com>""<wyq_l@qq.com>"
 __license__ = "GPL V3"
 __version__ = "0.1"
 
@@ -13,7 +13,7 @@ Use this script by :
 $ chmod +x absPath/picAll.py    # make this script run-able
 $ crontab -e                    # write these cmd in
 
-30 8 * * * python absPath/picAll.py     # run 8:30 per day
+30 8 * * * python absPath/picAll.py     # run 8:30  am per day
 # add some more daily running
 ```
 
@@ -31,20 +31,19 @@ import sys,time
 
 # without package management in version-0.1 , we need to
 # append system path for our scripts
-sys.path.append("/home/magic-board/Desktop/AgRobot-master/src/management_scripts/")
-sys.path.append("/home/magic-board/Desktop/AgRobot-master/src/tools/")
+sys.path.append("/home/pi/Desktop/AgRobot/src/management_scripts/")
+sys.path.append("/home/pi/Desktop/AgRobot/src/")
 
 from management_scripts import navigator
 # import python script by absolute path due to using crontab
 
 from tools import imgRec
+STEP_X = -180  # each step for 12m side in mm
+STEP_Y = 360  # each step for 1m side in mm
 
-STEP_X = -500  # each step for 12m side in mm
-STEP_Y = -70  # each step for 1m side in mm
 
-
-WALK_X = 2  # max walks for 12m side
-WALK_Y = 2  # max walks for 1m side
+WALK_X = 30  # max walks for 12m side
+# WALK_Y = 2  # max walks for 1m side
 
 # init a recoder to take and join photos
 Recoder = imgRec.ImgRec()
@@ -59,8 +58,8 @@ nav = navigator.Navigator()
 # (1,0.75) (1.0,0.5) .....  (11.5,0.75)
 
 w_x = 0  # current walk in x
-w_y = 0  # current walk in y
-dir_y = 1  # direction for y
+w_y = 320  # current walk in y
+dir_y = -1  # direction for y
 
 # when finished walking in y , change dir NEXT TIME
 _preChange_y = True
@@ -76,21 +75,29 @@ what a beautiful art , (ฅ•-•ฅ) , isn't it?
 # use a boolean to control robot not to turn back
 # immediately when finished y
 for w_x in range(WALK_X):
-    for w_y in range(WALK_Y):
-        # new dir has been set but not move when it start
-        if _preChange_y:
-            _preChange_y = False
-        else:
-            nav.move_y(dir_y * STEP_Y)
-        Recoder.capture_frame()
-    _preChange_y = True
-    # set the contrary of original direction
-    dir_y *= -1
+    
+    if (w_x % 2==0):
+        Recoder.capture_frame(None,w_x,0)
+        time.sleep(3)
+        nav.move_y(dir_y*STEP_Y)
+        time.sleep(4)
+        Recoder.capture_frame(None,w_x,1)
+    else:
+        Recoder.capture_frame(None,w_x,1)
+        time.sleep(3)
+        nav.move_y(dir_y*STEP_Y)
+        time.sleep(4)
+        Recoder.capture_frame(None,w_x,0)
+    dir_y = dir_y * -1
+    time.sleep(3)
     nav.move_x(STEP_X)
+    # log('20190521_14/20/43  move_Y_from_?_to_?  success/failed ')
+
 
 
 # it will close and release cameras' resources by itself
 del Recoder
+del nav
 
 
 

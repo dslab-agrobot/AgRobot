46ac19d811900fd9e891efdeed881a9b90e1863e
WeiYongqiang55 <wyq_l@qq.com>
2019-05-22 17:23:24 +0800

    add param path to capture frame

--- src/management_scripts/picAll.py
+++ src/management_scripts/picAll.py
@@ -28,12 +28,12 @@ Edit crontab with 'crontab -e' as below, then it can be executed automatic
 """
 
 import sys,time
-
 # without package management in version-0.1 , we need to
 # append system path for our scripts
 sys.path.append("/home/pi/Desktop/AgRobot/src/management_scripts/")
 sys.path.append("/home/pi/Desktop/AgRobot/src/")
-
+sys.path.append("/home/pi/Desktop/AgRobot/src/tools")
+from tools import dir
 from management_scripts import navigator
 # import python script by absolute path due to using crontab
 
@@ -41,7 +41,8 @@ from tools import imgRec
 STEP_X = -180  # each step for 12m side in mm
 STEP_Y = 360  # each step for 1m side in mm
 
-
+# current file path
+base_path='./'
 WALK_X = 30  # max walks for 12m side
 # WALK_Y = 2  # max walks for 1m side
 
@@ -50,7 +51,10 @@ Recoder = imgRec.ImgRec()
 
 # init a navigator to control robot
 nav = navigator.Navigator()
-
+# pics saved path
+path=base_path+dir.cur_time()
+#mkdir new path for saving pics
+dir.mkdir(path)
 # aimed at saving energy , we decided to move robot's y-aix
 # more often , and move like by S route.
 # S route for (x,y) like :
@@ -77,31 +81,22 @@ what a beautiful art , (ฅ•-•ฅ) , isn't it?
 for w_x in range(WALK_X):
     
     if (w_x % 2==0):
-        Recoder.capture_frame(None,w_x,0)
+        Recoder.capture_frame(None,w_x,0,path)
         time.sleep(3)
         nav.move_y(dir_y*STEP_Y)
         time.sleep(4)
-        Recoder.capture_frame(None,w_x,1)
+        Recoder.capture_frame(None,w_x,1,path)
     else:
-        Recoder.capture_frame(None,w_x,1)
+        Recoder.capture_frame(None,w_x,1,path)
         time.sleep(3)
         nav.move_y(dir_y*STEP_Y)
         time.sleep(4)
-        Recoder.capture_frame(None,w_x,0)
+        Recoder.capture_frame(None,w_x,0,path)
     dir_y = dir_y * -1
     time.sleep(3)
     nav.move_x(STEP_X)
     # log('20190521_14/20/43  move_Y_from_?_to_?  success/failed ')
 
-
-
 # it will close and release cameras' resources by itself
 del Recoder
 del nav
-
-
-
-
-
-
-

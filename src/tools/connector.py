70f935b4dddf53790d8e2989d4630faf9204b40d
JChrysanthemum <624099049@qq.com>
2019-05-30 09:31:26 +0800

    update logic of connector

--- src/tools/connector.py
+++ src/tools/connector.py
@@ -1,7 +1,31 @@
-#coding=utf-8
+# -*-encoding=utf-8-*-
+"""
+Navigator to control robot to move and act
+
+
+__copyright__="Liuxin","Jiangxt"
+__email__ = "<jiangxt404@qq.com>"
+__license__ = "GPL V3"
+__version__ = "0.2"
+
+
+Use ths script by:
+```
+from connector import send_msg
+
+# Use send_msg with implicit name means to send to 'move'
+send_msg('X+0043;')
+send_msg('Y+028;')
+
+```
+
+
+"""
 import os
 import time
 import serial
+
+MAX_RETRY = 10
 
 
 def __init(name=None):
@@ -11,50 +35,71 @@ def __init(name=None):
         dev = os.popen("ls -l /dev/ttyUSB* | awk '{print $10}'")
     dev = dev.readline().strip()
     ser = None
-    try:
-        ser = serial.Serial(dev, 115200, timeout=3)
-        time.sleep(2)
-    except Exception as e:
-        print ('[Error] Failed to connect arduino. Reason:', e)
-        if ser:
-            ser.close()
-            ser = serial.Serial(dev, 115200, timeout=16)
-    finally:
-        pass
+
+    # We can't find this device sometimes , so having some retry can fix it
+    for cnt in range(MAX_RETRY):
+        try:
+            cnt += 1
+            ser = serial.Serial(dev, 115200, timeout=3)
+
+            # We need this sleep for reacting
+            time.sleep(2)
+            break
+        except Exception as e:
+            print('\r\n [Error] Failed to connect arduino. Reason:', e)
+            if cnt < MAX_RETRY:
+                print('Retrying connecting [%d]')
+        finally:
+            if ser:
+                ser.close()
+
     return ser
 
 
-def send_msg(msg, name = None):
+def send_msg(msg, name=None):
     ser = __init(name)
-    for i in range(100):
-        try:
-            print(i)
-            time.sleep(1)
-            ser.write(msg)
-            break
-        except Exception as e:
-            print("ser write msg error", e)
-            if ser:
-               ser.close()
-               ser = __init()
-        finally:
-            pass
+    cnt = 0
+    try:
+        cnt += 1
+        time.sleep(1)
+        ser.write(msg)
+    except Exception as e:
+        print("\r\n [Error] Failed to send message. Reason:", e)
+        if cnt < MAX_RETRY:
+            print('Try to create a new connection')
+    finally:
+        if ser:
+            ser.close()
+            ser = __init()
+
     msg = None
-    for i in range(100):
-        try:
-            msg = ser.read_until('!!!')
-            break 
-        except Exception as e:
-            print("ser read msg error", e)
-            if ser:
-                ser.close()
-                ser = __init()
-        finally:
-            pass
+    try:
+        msg = ser.read_until('!!!')
+    except Exception as e:
+        print("[Error] Failed to send message. Reason: %s \n"
+              "Useless for retrying to read or create new one", e)
+    finally:
+        if ser:
+            ser.close()
+            ser = __init()
+
     if ser:
         ser.close()
     return msg
 
 
 if __name__ == '__main__':
-    print (send_msg('X+0043;'))
+    print(send_msg('X+0043;'))
+
+"""
+Indeed , there are still two problems rarely happen [ occurs less than 5% ],
+and we have no good idea for these 
+    1) Electronic interference or something else may influence the sending
+        of serial port . It means we send all part of message but device just
+        receive part of them . [Y moves wried distance twice with correct cmd and
+        correct return]
+    
+    2) Arduino just return its message once without confirmation . So sometimes we
+        can't read message that can do nothing but raise a error. (Retrying to read
+        message from serial port is stupid . )
+"""
